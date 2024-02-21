5a65f2d8218eab2f674dd0e1d989b1929073f188
Ihsanurdin <isan7964@gmail.com>
2024-02-21 13:30:08 +0700

    Projek Baru

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("Tiket Bioskop");
+
+const nama = prompt("Masukkan nama anda : ");
+const umur = prompt("Masukkan umur anda : ");
+
+if (umur <= 13) {
+  alert(`${nama}, Tidak Boleh Masuk Studio Karena Belum Cukup Umur`);
+} else {
+  const studio = prompt("Pilih studio A, B, atau C (Huruf Besar) : ")
+  
+  if (studio == "A") {
+    alert(`Tiket ${nama} , Studio A, Umur ${umur}`);
+  } else if (studio == "B") {
+    alert(`Tiket ${nama} , Studio B, Umur ${umur}`);
+  } else if (studio == "C") {
+    alert(`Tiket ${nama} , Studio C, Umur ${umur}`);
+  }
+}
