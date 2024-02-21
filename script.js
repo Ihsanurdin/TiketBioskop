a85445cf55c4dec6f4764dc58d01b02204f8a74e
Ihsanurdin <156874596+Ihsanurdin@users.noreply.github.com>
2024-02-21 13:42:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ alert("Tiket Bioskop");
 const nama = prompt("Masukkan nama anda : ");
 const umur = prompt("Masukkan umur anda : ");
 
-if (umur <= 13) {
+if (umur >= 13) {
   alert(`${nama}, Tidak Boleh Masuk Studio Karena Belum Cukup Umur`);
 } else {
   const studio = prompt("Pilih studio A, B, atau C (Huruf Besar) : ")
